f05c43d867cd1e4574021431b30091cd84ff7108
Simon Leischnig <simon.leischnig@dlr.de>
2018-07-20 18:40:29 +0200

    loading drafts etc works

--- interactive.sh
+++ interactive.sh
@@ -1,4 +1,6 @@
 mod_composure_repo="$mod_composure_root/composure_home"
+
+LOAD_COMPOSED_FUNCTIONS=0
 source "$mod_composure_sw/composure.sh"
 
 #added -follow
@@ -6,3 +8,5 @@ _list_composure_files () {
   typeset composure_dir="$(_get_composure_dir)"
   [ -d "$composure_dir" ] && find "$composure_dir" -follow -maxdepth 1 -name '*.inc'
 }
+LOAD_COMPOSED_FUNCTIONS=1
+_bootstrap_composure
